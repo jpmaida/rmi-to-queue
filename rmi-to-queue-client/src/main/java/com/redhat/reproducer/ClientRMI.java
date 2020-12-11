7d4944df490a107a63afbc9147d92c23d75acac9
João Paulo Maida <jpmaida@gmail.com>
2020-12-11 14:57:21 -0300

    Arquivos renomeados e classe de teste apagada.

--- rmi-to-queue-client/src/main/java/com/redhat/reproducer/ClientRMI.java
+++ rmi-to-queue-client/src/main/java/com/redhat/reproducer/ClientRMI.java
@@ -2,24 +2,21 @@ package com.redhat.reproducer;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.logging.Logger;
 
-/**
- * Hello world!
- *
- */
 public class Client
 {
+    private final static Logger LOGGER = Logger.getLogger(Client.class.getName());
     public static void main( String[] args )
     {
-        String host = (args.length < 1) ? null : args[0];
         try {
-            Registry registry = LocateRegistry.getRegistry(host, 5000);
-            Hello stub = (Hello) registry.lookup("Hello");
+            Registry registry = LocateRegistry.getRegistry("127.0.0.1", 5000);
+            HelloMessage stub = (HelloMessage) registry.lookup("HelloMessage");
             String response = stub.sayHello();
             System.out.println("response: " + response);
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
-            e.printStackTrace();
+            LOGGER.severe(e.getMessage());
         }
     }
-}
+}®
--- rmi-to-queue-client/src/main/java/com/redhat/reproducer/HelloMessage.java
+++ rmi-to-queue-client/src/main/java/com/redhat/reproducer/HelloMessage.java
@@ -3,6 +3,6 @@ package com.redhat.reproducer;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface Hello extends Remote {
+public interface HelloMessage extends Remote {
     String sayHello() throws RemoteException;
 }
